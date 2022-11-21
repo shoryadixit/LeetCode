318292a1dee8162d1e0f84a1d02f13bca19cc5d8
Shorya Dixit <shauryadt123@gmail.com>
2022-11-21 19:26:04 +0530

    Time: 6 ms (96.73%), Space: 43.1 MB (99.11%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.java
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.java
@@ -1,31 +1,40 @@
 class Solution {
+     public boolean isValidRoom(int row, int col, char[][] maze) {
+		if (row < 0 || row >= maze.length || col < 0 || col >= maze[0].length || maze[row][col] == '+') {
+			return false;
+		}
+		maze[row][col] = '+';
+		return true;
+	}
+    
     public int nearestExit(char[][] maze, int[] entrance) {
-        int rows = maze.length, cols = maze[0].length;
-        int[][] dirs = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-        
-        int startRow = entrance[0], startCol = entrance[1];
-        maze[startRow][startCol] = '+';
-        
         Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{startRow, startCol, 0});
-        
+        queue.offer(entrance);
+        maze[entrance[0]][entrance[1]] = '+';
+        int steps = 0;
         while (!queue.isEmpty()) {
-            int[] currState = queue.poll();
-            int currRow = currState[0], currCol = currState[1], currDistance = currState[2];
-
-            for (int[] dir : dirs) {
-                int nextRow = currRow + dir[0], nextCol = currCol + dir[1];
-
-                if (0 <= nextRow && nextRow < rows && 0 <= nextCol && nextCol < cols
-                   && maze[nextRow][nextCol] == '.') {
-                    
-                    if (nextRow == 0 || nextRow == rows - 1 || nextCol == 0 || nextCol == cols - 1)
-                        return currDistance + 1;
-                    
-                    maze[nextRow][nextCol] = '+';
-                    queue.offer(new int[]{nextRow, nextCol, currDistance + 1});
-                }  
-            }
+            int size = queue.size();
+			for (int i = 0; i < size; i++) {
+				int[] rowCol = queue.poll();
+                int row = rowCol[0];
+                int col = rowCol[1];
+				if (isValidRoom(row + 1, col, maze)) {
+					queue.add(new int[]{row + 1, col});
+				}
+				if (isValidRoom(row - 1, col, maze)) {
+					queue.add(new int[]{row - 1, col});
+				}
+                if (isValidRoom(row, col + 1, maze)) {
+					queue.add(new int[]{row, col + 1});
+				}
+				if (isValidRoom(row, col - 1, maze)) {
+					queue.add(new int[]{row, col - 1});
+				}
+                if ((row == 0 || row == maze.length - 1 || col == 0 || col == maze[0].length - 1) && (row != entrance[0] || col != entrance[1])){ 
+                    return steps;
+                }
+			}
+            steps++;
         }
         return -1;
     }
