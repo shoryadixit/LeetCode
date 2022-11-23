21e56ed0e8839e6d947b873f8cf322781f015edb
Shorya Dixit <shauryadt123@gmail.com>
2022-11-23 21:41:14 +0530

    Time: 23 ms (30.67%), Space: 49.2 MB (5.89%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,15 +1,42 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        Set seen = new HashSet();
-        for (int i=0; i<9; ++i) {
-            for (int j=0; j<9; ++j) {
-                if (board[i][j] != '.') {
-                    String b = "(" + board[i][j] + ")";
-                    if (!seen.add(b + i) || !seen.add(j + b) || !seen.add(i/3 + b + j/3))
-                        return false;
+        Map<Integer, Set<Character>> rows = new HashMap<>();
+        Map<Integer, Set<Character>> cols = new HashMap<>();
+        Map<String, Set<Character>> grids = new HashMap<>();
+
+        for(int row=0; row<board.length; row++) {
+            for(int col=0; col < board[0].length; col++) {
+                if(board[row][col]=='.') {
+                    continue;
+                }
+
+                // row check
+                if(rows.containsKey(row) && rows.get(row).contains(board[row][col])) {
+                    return false;
+                } else {
+                    rows.putIfAbsent(row, new HashSet<>());
+                    rows.get(row).add(board[row][col]);
+                }
+
+                // col check
+                if(cols.containsKey(col) && cols.get(col).contains(board[row][col])) {
+                    return false;
+                } else {
+                    cols.putIfAbsent(col, new HashSet<>());
+                    cols.get(col).add(board[row][col]);
+                }
+
+                // 3x3 grid check
+                String gridId = row/3 + "" + col/3;
+                if(grids.containsKey(gridId) && grids.get(gridId).contains(board[row][col])) {
+                    return false;
+                } else {
+                    grids.putIfAbsent(gridId, new HashSet<>());
+                    grids.get(gridId).add(board[row][col]);
                 }
             }
         }
+        
         return true;
     }
 }
