b2b315dc2608cd6cdea7aa2e363ff64127b92843
Shorya Dixit <shauryadt123@gmail.com>
2022-11-24 22:37:55 +0530

    Time: 189 ms (50.29%), Space: 42.7 MB (23.74%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,31 +1,28 @@
 class Solution {
-    public boolean exist(char[][] board, String word){
-    if (board[0].length == 0 || board.length == 0 || word.length() == 0) return false; 
-    int n = board[0].length, m = board.length;
-    for (int i = 0; i < m; i++){
-        for(int j = 0; j < n; j++){
-            if (board[i][j] == word.charAt(0) && DFS(board, word, i, j, 0, new boolean[board.length][board[i].length]))
-                return true;
+public boolean exist(char[][] board, String word) {
+        if (board == null || board.length == 0 || board[0].length == 0 || word == null || word.equals("")) {
+            return false;
         }
-    }
-    return false;
-}
-
-public boolean DFS (char [][] board,String word, int i, int j, int k, boolean [][] visited){
-    if (k == word.length()) return true; 
-    
-    if(i < 0 || j < 0 || j >=board[0].length 
-       || i >= board.length || word.charAt(k) != board[i][j] || visited[i][j]) 
+        for (int i = 0; i < board.length; i ++) {
+            for (int j = 0; j < board[0].length; j ++) {
+                if (search(board, word, i, j, 0) == true) {
+                    return true;
+                }
+            }
+        }
         return false;
-        
-    visited[i][j] = true;
-    if(DFS(board, word, i-1, j, k+1, visited) || 
-       DFS(board, word, i+1, j, k+1, visited) || 
-       DFS(board, word, i, j-1, k+1, visited) ||
-       DFS(board, word, i, j+1, k+1, visited))
-        return true;
-    
-    visited[i][j] = false;
-    return false;
-}
+    }
+    private boolean search(char[][] board, String word, int i, int j, int matched) {
+        if (matched == word.length()) {
+            return true;
+        }
+        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length || board[i][j] != word.charAt(matched)) {
+            return false;
+        }
+        board[i][j] = '#';
+        boolean result = search(board, word, i - 1, j, matched + 1) || search(board, word, i, j - 1, matched + 1) || search(board, word, i + 1, j, matched + 1) ||  search(board, word, i, j + 1, matched + 1);
+        board[i][j] = word.charAt(matched);
+        return result;
+
+    }
 }
