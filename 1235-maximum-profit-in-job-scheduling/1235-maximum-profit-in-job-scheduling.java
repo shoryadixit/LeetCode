1f4b91a91750d85150ea550913bf74081bb7fe66
Shorya Dixit <shauryadt123@gmail.com>
2022-11-26 22:07:01 +0530

    Time: 45 ms (82.62%), Space: 72.7 MB (57.52%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
@@ -1,18 +1,29 @@
 class Solution {
     public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
-        int n = startTime.length;
-        int[][] jobs = new int[n][3];
-        for (int i = 0; i < n; i++) {
-            jobs[i] = new int[] {startTime[i], endTime[i], profit[i]};
+        int[][] jobs = new int[startTime.length][3];
+        for (int i = 0; i < startTime.length; i++) {
+            jobs[i] = new int[]{startTime[i], endTime[i], profit[i]};
         }
-        Arrays.sort(jobs, (a, b)->a[1] - b[1]);
-        TreeMap<Integer, Integer> dp = new TreeMap<>();
-        dp.put(0, 0);
-        for (int[] job : jobs) {
-            int cur = dp.floorEntry(job[0]).getValue() + job[2];
-            if (cur > dp.lastEntry().getValue())
-                dp.put(job[1], cur);
+        Arrays.sort(jobs, (a, b) -> a[0] - b[0]);
+        
+        int[] dp = new int[jobs.length];
+        dp[jobs.length - 1] = jobs[jobs.length - 1][2];
+        for (int cur = jobs.length - 2; cur >= 0; cur--) {
+            int next = findNext(cur, jobs);
+            dp[cur] = Math.max(
+                jobs[cur][2] + (next == -1 ? 0 : dp[next]),
+                dp[cur + 1]
+            );
         }
-        return dp.lastEntry().getValue();
+        return dp[0];
+    }
+    
+    private int findNext(int cur, int[][] jobs) {
+        for (int next = cur + 1; next < jobs.length; next++) {
+            if (jobs[next][0] >= jobs[cur][1]) {
+                return next;   
+            }
+        }
+        return -1;
     }
 }
