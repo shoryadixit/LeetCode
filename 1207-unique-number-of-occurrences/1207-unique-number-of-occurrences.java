82a5f4a7296cd168a18920a0fc6ee6f75ae35b8f
Shorya Dixit <shauryadt123@gmail.com>
2022-11-30 22:21:02 +0530

    Time: 9 ms (6.76%), Space: 42.9 MB (6.00%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
@@ -1,12 +1,16 @@
 class Solution {
-    public boolean uniqueOccurrences(int[] arr) {
-        Map<Integer, Integer> freq = new HashMap<>();
-        for (int num : arr) {
-            freq.put(num, freq.getOrDefault(num, 0) + 1);
-        }
-        
-        Set<Integer> freqSet = new HashSet<>(freq.values());
-
-        return freq.size() == freqSet.size();
-    }
+     public boolean uniqueOccurrences(int[] arr) {
+         int[] c = new int[2001];
+         for (int n : arr) {
+             c[n+1000]++;
+         }
+         Set<Integer> set = new HashSet();
+         for (int count : c) {
+             if (count == 0)
+                 continue;
+             if(!set.add(count))
+                 return false;
+         }
+         return true;
+     }
 }
