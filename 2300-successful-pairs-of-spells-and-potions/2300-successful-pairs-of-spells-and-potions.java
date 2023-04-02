35dac68cdf29767cfd40aa02aa6ee8421ef0bbc3
Shorya Dixit <shauryadt123@gmail.com>
2023-04-02 22:59:44 +0530

    Time: 73 ms (74.38%), Space: 62 MB (50.83%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.java
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.java
@@ -1,33 +1,25 @@
 class Solution {
-    public int[] successfulPairs(int[] spells, int[] potions, long success) {
-        int n = spells.length;
-        int m = potions.length;
-        
-        // Create an array of pairs containing spell and its original index
-        int[][] sortedSpells = new int[n][2];
-        for (int i = 0; i < n; i++) {
-            sortedSpells[i][0] = spells[i];
-            sortedSpells[i][1] = i;
-        }
-        
-        // Sort the 'spells with index' and 'potions' array in increasing order
-        Arrays.sort(sortedSpells, (a, b) -> Integer.compare(a[0], b[0]));
-        Arrays.sort(potions);
-        
-        // For each 'spell' find the respective 'minPotion' index
-        int[] answer = new int[n];
-        int potionIndex = m - 1;
+  public int[] successfulPairs(int[] spells, int[] potions, long success) {
+    int[] ans = new int[spells.length];
+    Arrays.sort(potions);
 
-        for (int[] sortedSpell : sortedSpells) {
-            int spell = sortedSpell[0];
-            int index = sortedSpell[1];
-            
-            while (potionIndex >= 0 && (long) spell * potions[potionIndex] >= success) {
-                potionIndex -= 1;
-            }
-            answer[index] = m - (potionIndex + 1);
-        }
-        
-        return answer;
+    for (int i = 0; i < spells.length; ++i)
+      ans[i] = potions.length - firstIndexSuccess(spells[i], potions, success);
+
+    return ans;
+  }
+
+  // First index i s.t. spell * potions[i] >= success
+  private int firstIndexSuccess(int spell, int[] potions, long success) {
+    int l = 0;
+    int r = potions.length;
+    while (l < r) {
+      final int m = (l + r) / 2;
+      if ((long) spell * potions[m] >= success)
+        r = m;
+      else
+        l = m + 1;
     }
+    return l;
+  }
 }
