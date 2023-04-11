fc31834e4468f2405b40dde2447ced35e9b9e2b1
Shorya Dixit <shauryadt123@gmail.com>
2023-04-11 23:36:37 +0530

    Time: 47 ms (80.61%), Space: 43.4 MB (84.49%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
@@ -1,18 +1,38 @@
+// Using stack----
+
+// class Solution {
+//     public String removeStars(String s) {
+//         Stack<Character> st = new Stack<>();
+//         for (int i = 0; i < s.length(); i++) {
+//             if(s.charAt(i) == '*') {
+//                 st.pop();
+//             } else {
+//                 st.push(s.charAt(i));
+//             }
+//         }
+//         String ans = "";
+//         while (!st.isEmpty()) {
+//             char ele = st.pop();
+//             ans = ele + ans;
+//         }
+//         return ans;
+//     }
+// }
+
+
+//Using String----
+
 class Solution {
     public String removeStars(String s) {
-        Stack<Character> st = new Stack<>();
-        for (int i = 0; i < s.length(); i++) {
-            if(s.charAt(i) == '*') {
-                st.pop();
+        int j = 0;
+        StringBuilder ans = new StringBuilder();
+        for (int i  = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '*') {
+                ans.deleteCharAt(ans.length() - 1);
             } else {
-                st.push(s.charAt(i));
+                ans.append(s.charAt(i));
             }
         }
-        String ans = "";
-        while (!st.isEmpty()) {
-            char ele = st.pop();
-            ans = ele + ans;
-        }
-        return ans;
+        return ans.toString();
     }
 }
