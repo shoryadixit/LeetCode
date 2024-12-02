ad02c8dc100f0b8f4b743f62a122bf3eb95835a6
Shorya Dixit <shauryadt123@gmail.com>
2024-12-02 17:09:10 +0530

    Time: 1 ms (92.20%), Space: 46.1 MB (19.29%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,11 +1,9 @@
 class Solution {
     public static int solve(int index, int buy, int[] prices, int[][] dp) {
-        // Base case: if index exceeds or matches the length of the array, no profit can be made
         if (index == prices.length) {
             return 0;
         }
 
-        // Check if the result is already computed
         if (dp[index][buy] != -1) {
             return dp[index][buy];
         }
@@ -13,31 +11,52 @@ class Solution {
         int profit = 0;
 
         if (buy == 1) {
-            // Either buy the stock or skip
             int buying = -prices[index] + solve(index + 1, 0, prices, dp);
             int skipping = solve(index + 1, 1, prices, dp);
             profit = Math.max(buying, skipping);
         } else {
-            // Either sell the stock or skip
             int selling = prices[index] + solve(index + 1, 1, prices, dp);
             int skipping = solve(index + 1, 0, prices, dp);
             profit = Math.max(selling, skipping);
         }
 
-        // Store the result in dp table
         dp[index][buy] = profit;
         return profit;
     }
     
-    public static int tabulationMethod() {
-        return 0;
+    public static int tabulationMethod(int[] prices) {
+        int[][] dp = new int[prices.length + 1][2];
+        for (int i = 0; i < dp.length; i++) {
+            Arrays.fill(dp[i], 0);
+        }
+        
+        for (int i = prices.length - 1; i >= 0; i--) {
+            for (int j = 0; j <= 1; j++) {
+                int profit = 0;
+
+                if (j == 1) {
+                    int buying = -prices[i] + dp[i + 1][0];
+                    int skipping = dp[i + 1][1];
+                    profit = Math.max(buying, skipping);
+                } else {
+                    int selling = prices[i] + dp[i + 1][1];
+                    int skipping = dp[i + 1][0];
+                    profit = Math.max(selling, skipping);
+                }
+                
+                dp[i][j] = profit;
+            }
+        }
+        return dp[0][1];
     }
     
     public int maxProfit(int[] prices) {
-        int[][] dp = new int[prices.length][2];
-        for (int i = 0; i < dp.length; i++) {
-            Arrays.fill(dp[i], -1);
-        }
-        return solve(0, 1, prices, dp);
+        // int[][] dp = new int[prices.length][2];
+        // for (int i = 0; i < dp.length; i++) {
+        //     Arrays.fill(dp[i], -1);
+        // }
+        // return solve(0, 1, prices, dp);
+        
+        return tabulationMethod(prices);
     }
 }
